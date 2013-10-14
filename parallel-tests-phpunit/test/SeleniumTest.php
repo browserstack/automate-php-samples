9a157fd244a8e43d671db00242c0f30992ad2309
Akshay Bhardwaj <akshay@browserstack.com>
2013-10-14 15:36:31 +0530

    Streamed code

--- parallel-tests-phpunit/test/SeleniumTest.php
+++ parallel-tests-phpunit/test/SeleniumTest.php
@@ -2,10 +2,22 @@
 require 'vendor/autoload.php';
 
 class SeleniumTest extends PHPUnit_Framework_TestCase {
+
+ protected static $user_id = "<Your Userid>";
+ protected static $security_key = "<Your Security Key>";
+ 
  public function testGoogle() {
+  $url = "http://" . self::$user_id . ":" . self::$security_key . "@hub.browserstack.com/wd/hub";
+  $caps = array(
+    "browser" => "IE",
+    "browser_version" => "11.0",
+    "os" => "Windows",
+    "os_version" => "8.1",
+    "browserstack.debug" => "true"    
+  );
   $web_driver = new RemoteWebDriver(
-    "http://akshaybhardwaj1:XQWDewaJsUzqYJRv8zhr@hub.browserstack.com/wd/hub", 
-    array("platform"=>"WINDOWS")
+    $url,
+    $caps
   );
   $web_driver->get("http://www.google.com");
   print $web_driver->getTitle();
@@ -17,18 +29,34 @@ class SeleniumTest extends PHPUnit_Framework_TestCase {
   $web_driver->quit();
  }
  public function testBrowserStack() {
+  $url = "http://" . self::$user_id . ":" . self::$security_key . "@hub.browserstack.com/wd/hub";
+  $caps = array(
+    "browser" => "IE",
+    "browser_version" => "11.0",
+    "os" => "Windows",
+    "os_version" => "8",
+    "browserstack.debug" => "true"    
+  );
   $web_driver = new RemoteWebDriver(
-    "http://akshaybhardwaj1:XQWDewaJsUzqYJRv8zhr@hub.browserstack.com/wd/hub", 
-    array("platform"=>"WINDOWS")
+    $url,
+    $caps
   );
   $web_driver->get("http://www.browserstack.com");
   print $web_driver->getTitle();
   $web_driver->quit();
  }
  public function testFacebook() {
+  $url = "http://" . self::$user_id . ":" . self::$security_key . "@hub.browserstack.com/wd/hub";
+  $caps = array(
+    "browser" => "IE",
+    "browser_version" => "11.0",
+    "os" => "Windows",
+    "os_version" => "8.1",
+    "browserstack.debug" => "true"    
+  );
   $web_driver = new RemoteWebDriver(
-    "http://akshaybhardwaj1:XQWDewaJsUzqYJRv8zhr@hub.browserstack.com/wd/hub", 
-    array("platform"=>"WINDOWS")
+    $url,
+    $caps
   );
   $web_driver->get("http://www.facebook.com");
   print $web_driver->getTitle();
